5060d2e16fba03a889311abc1053b2560096aea9
Nate <nathan.sampo@students.olin.edu>
2018-09-11 14:19:58 -0400

    Worked on teleop

--- scripts/drive_square.py
+++ scripts/drive_square.py
@@ -1,47 +1,39 @@
-#!/usr/bin/env python
-""" This script drives a robot in a 1m x 1m square """
-import rospy
-from geometry_msgs.msg import Twist
+import tty
+import select
+import sys
+import termios
+import interface
+import Xlib.display as display
 
-class square_driver(object):
+win32gui.GetCursorPos(point)
 
-	def __init__(self):
-		rospy.init_node('bumpty_dumpty')
-		self.publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
-		self.rate = rospy.Rate(10)
-		self.vel_msg = Twist()
-		self.vel_msg.linear.x = 0
-		self.vel_msg.angular.z = 0
+class TeleopC(object):
+    def __init__ (self):
+        self.settings = termios.tcgetattr(sys.stdin)
+        self.myspeedctrl = interface.SendSpeed()
+        self.offsetX = display.Display().screen().root.query_pointer()._data['root_x']
+        self.offsetY = display.Display().screen().root.query_pointer()._data['root_y']
 
-		# set parameters
-		self.time_until_turn = 3.2
-		self.time_for_turn = 1.57
-
-	def run(self):
-		while not rospy.is_shutdown():
-			self.move_straight()
-			self.turn()
-			self.rate.sleep()
-
-	def move_straight(self):
-		time_started_moving_straight = rospy.Time.now()
-		time_stop_moving_straight = time_started_moving_straight + rospy.Duration(self.time_until_turn)
-		self.vel_msg.linear.x = 1
-		self.vel_msg.angular.z = 0
-
-		while rospy.Time.now() < time_stop_moving_straight:
-			self.publisher.publish(self.vel_msg)
-
-	def turn(self):
-		time_started_turning = rospy.Time.now()
-		time_stop_turning = time_started_turning + rospy.Duration(self.time_for_turn)
-		self.vel_msg.linear.x = 0
-		self.vel_msg.angular.z = -1
-
-		while rospy.Time.now() < time_stop_turning:
-			self.publisher.publish(self.vel_msg)
+    def getDirectionUnit(self):
 
 
-if __name__ == '__main__':
-	node = square_driver()
-    node.run()
+    def run(self):
+        while key != '\x03':
+            self.key = self.getKey()
+            if self.key == "a":
+                self.myspeedctrl.send_speed(0,1)
+            elif self.key =="s":
+                self.myspeedctrl.send_speed(-1,0)
+            elif self.key == "w":
+                self.myspeedctrl.send_speed(1,0)
+            elif self.key == "d":
+                self.myspeedctrl.send_speed(0,-1)
+            elif self.key == ' ':
+                self.myspeedctrl.send_speed(0,0)
+            elif self.key == 'l':
+                self.myspeedctrl.low_rider()
+
+
+if __name__ == "__main__":
+    myTeleop = TeleopC()
+    myTeleop.run()
--- scripts/teleop.py
+++ scripts/teleop.py
@@ -2,17 +2,37 @@ import tty
 import select
 import sys
 import termios
+import interface
+import Xlib.display as display
 
-def getKey():
-    tty.setraw(sys.stdin.fileno())
-    select.select([sys.stdin], [], [], 0)
-    key = sys.stdin.read(1)
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key
+win32gui.GetCursorPos(point)
 
-settings = termios.tcgetattr(sys.stdin)
-key = None
+class TeleopC(object):
+    def __init__ (self):
+        self.myspeedctrl = interface.SendSpeed()
+        self.offsetX = display.Display().screen().root.query_pointer()._data['root_x']
+        self.offsetY = display.Display().screen().root.query_pointer()._data['root_y']
 
-while key != '\x03':
-    key = getKey()
-    print key
+    def getDirectionUnit(self):
+
+
+    def run(self):
+        while key != '\x03':
+            self.key = self.getKey()
+            if self.key == "a":
+                self.myspeedctrl.send_speed(0,1)
+            elif self.key =="s":
+                self.myspeedctrl.send_speed(-1,0)
+            elif self.key == "w":
+                self.myspeedctrl.send_speed(1,0)
+            elif self.key == "d":
+                self.myspeedctrl.send_speed(0,-1)
+            elif self.key == ' ':
+                self.myspeedctrl.send_speed(0,0)
+            elif self.key == 'l':
+                self.myspeedctrl.low_rider()
+
+
+if __name__ == "__main__":
+    myTeleop = TeleopC()
+    myTeleop.run()
