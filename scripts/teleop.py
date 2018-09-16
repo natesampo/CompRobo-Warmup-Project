d4cf5651097f67671708305ac55f94f3332df207
Nate <nathan.sampo@students.olin.edu>
2018-09-16 15:42:56 -0400

    robit drives now

--- scripts/teleop.py
+++ scripts/teleop.py
@@ -5,6 +5,10 @@ import select
 import sys
 import termios
 import rospy
+import os
+import thread
+import signal
+import math
 from std_msgs.msg import Header
 from geometry_msgs.msg import Twist, Vector3, Point, PointStamped
 from nav_msgs.msg import Odometry
@@ -17,23 +21,61 @@ def convert_pose_to_xy_and_theta(pose):
     angles = euler_from_quaternion(orientation_tuple)
     return (pose.position.x, pose.position.y, angles[2])
 
+def angle_normalize(z):
+    """ convenience function to map an angle to the range [-pi,pi] """
+    return math.atan2(math.sin(z), math.cos(z))
+
+def angle_diff(a, b):
+    """ Calculates the difference between angle a and angle b (both should be in radians)
+        the difference is always based on the closest rotation from angle a to angle b
+        examples:
+            angle_diff(.1,.2) -> -.1
+            angle_diff(.1, 2*math.pi - .1) -> .2
+            angle_diff(.1, .2+2*math.pi) -> -.1
+    """
+    a = angle_normalize(a)
+    b = angle_normalize(b)
+    d1 = a-b
+    d2 = 2*math.pi - math.fabs(d1)
+    if d1 > 0:
+        d2 *= -1.0
+    if math.fabs(d1) < math.fabs(d2):
+        return d1
+    else:
+        return d2
+
+def distanceTo(a, b):
+    return math.sqrt((a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]))
+
 def mouseToXY(mousePos):
-    return (mousePos[0]/700, mousePos[1]/700)
+    return (mousePos[0]/1500, mousePos[1]/1500)
 
 def getCurrentMousePosition():
     return (display.Display().screen().root.query_pointer()._data['root_x'], display.Display().screen().root.query_pointer()._data['root_y'])
 
+def input_thread(myTeleop):
+    running = True
+    while running:
+        '''Given code to grab keyboard values'''
+        tty.setraw(sys.stdin.fileno())
+        select.select([sys.stdin], [], [], 0)
+        key = sys.stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        if key == 'q':
+            running = False
+            os.kill(os.getpid(), signal.SIGINT)
+        elif key == 'a':
+            myTeleop.newOrigin()
+
 class TeleopC(object):
     def __init__ (self):
-        rospy.init_node("teleop")
+        rospy.init_node("teleop", disable_signals=True)
         self.publisher_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10, latch=True)
         self.odom_sub = rospy.Subscriber('odom', Odometry, self.getOdom)
         self.mouseOrigin = (display.Display().screen().root.query_pointer()._data['root_x'], display.Display().screen().root.query_pointer()._data['root_y'])
         self.velocityPublisher = rospy.Publisher('cmd_vel', Twist, queue_size=5)
         self.rate = rospy.Rate(10)
         self.vel = Twist()
-        self.vel.linear.x = 0
-        self.vel.angular.z = 0
         self.pose = (0, 0, 0)
         self.origin = self.pose
         self.getNewOrigin = True
@@ -53,10 +95,27 @@ class TeleopC(object):
 
     def run(self):
         while not rospy.is_shutdown():
-            #print(self.pose)
-            print(mouseToXY(self.getRelativeMousePosition(getCurrentMousePosition())))
+            currMouseOffset = (mouseToXY(self.getRelativeMousePosition(getCurrentMousePosition()))[0] - self.origin[0], mouseToXY(self.getRelativeMousePosition(getCurrentMousePosition()))[1] - self.origin[1])
+            if abs(angle_diff(self.pose[2], math.atan2(currMouseOffset[1]-self.pose[1], currMouseOffset[0]-self.pose[0]))) < 0.1:
+                if distanceTo(currMouseOffset, (self.pose[0], self.pose[1])) > 0.1:
+                    self.vel.linear.x = 1
+                    self.vel.angular.z = 0
+            else:
+                if math.atan2(currMouseOffset[1]-self.pose[1], currMouseOffset[0]-self.pose[0]) > 0:
+                    self.vel.angular.z = 1
+                else:
+                    self.vel.angular.z = -1
+                self.vel.linear.x = 0
 
+            self.velocityPublisher.publish(self.vel)
 
 if __name__ == "__main__":
+    settings = termios.tcgetattr(sys.stdin)
     myTeleop = TeleopC()
-    myTeleop.run()
+    thread.start_new_thread(input_thread, (myTeleop, ))
+    try:
+        myTeleop.run()
+    except KeyboardInterrupt:
+        myTeleop.vel.linear.x = 0
+        myTeleop.vel.angular.z = 0
+        myTeleop.velocityPublisher.publish(myTeleop.vel)
