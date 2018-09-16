2441d5a14563343fdbd502c4b7bcdde0707b4633
Nate <nathan.sampo@students.olin.edu>
2018-09-16 14:30:26 -0400

    Added changing origin

--- scripts/teleop.py
+++ scripts/teleop.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+from __future__ import division
 import tty
 import select
 import sys
 import termios
 import rospy
-from std_msgs.msg import Point
+from std_msgs.msg import Header
 from geometry_msgs.msg import Twist, Vector3, Point, PointStamped
 from nav_msgs.msg import Odometry
 import Xlib.display as display
@@ -16,39 +17,44 @@ def convert_pose_to_xy_and_theta(pose):
     angles = euler_from_quaternion(orientation_tuple)
     return (pose.position.x, pose.position.y, angles[2])
 
+def mouseToXY(mousePos):
+    return (mousePos[0]/700, mousePos[1]/700)
+
+def getCurrentMousePosition():
+    return (display.Display().screen().root.query_pointer()._data['root_x'], display.Display().screen().root.query_pointer()._data['root_y'])
+
 class TeleopC(object):
     def __init__ (self):
         rospy.init_node("teleop")
         self.publisher_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10, latch=True)
         self.odom_sub = rospy.Subscriber('odom', Odometry, self.getOdom)
-        self.settings = termios.tcgetattr(sys.stdin)
-        self.myspeedctrl = interface.SendSpeed()
-        self.offsetX = display.Display().screen().root.query_pointer()._data['root_x']
-        self.offsetY = display.Display().screen().root.query_pointer()._data['root_y']
+        self.mouseOrigin = (display.Display().screen().root.query_pointer()._data['root_x'], display.Display().screen().root.query_pointer()._data['root_y'])
         self.velocityPublisher = rospy.Publisher('cmd_vel', Twist, queue_size=5)
         self.rate = rospy.Rate(10)
         self.vel = Twist()
-        self.vel_msg.linear.x = 0
-        self.vel_msg.angular.z = 0
+        self.vel.linear.x = 0
+        self.vel.angular.z = 0
+        self.pose = (0, 0, 0)
+        self.origin = self.pose
+        self.getNewOrigin = True
 
     def getOdom(self, msg):
-        self.pose = convert_pose_to_xy_and_theta(msg.pose.pose);
+        self.pose = convert_pose_to_xy_and_theta(msg.pose.pose)
+        if self.getNewOrigin:
+            self.newOrigin()
+            self.getNewOrigin = False
+
+    def newOrigin(self):
+        self.origin = self.pose
+        self.mouseOrigin = getCurrentMousePosition()
+
+    def getRelativeMousePosition(self, mousePos):
+        return (mousePos[0] - self.mouseOrigin[0], mousePos[1] - self.mouseOrigin[1])
 
     def run(self):
-        while not rospy.is_shutdown:
-            self.key = self.getKey()
-            if self.key == "a":
-                self.myspeedctrl.send_speed(0,1)
-            elif self.key =="s":
-                self.myspeedctrl.send_speed(-1,0)
-            elif self.key == "w":
-                self.myspeedctrl.send_speed(1,0)
-            elif self.key == "d":
-                self.myspeedctrl.send_speed(0,-1)
-            elif self.key == ' ':
-                self.myspeedctrl.send_speed(0,0)
-            elif self.key == 'l':
-                self.myspeedctrl.low_rider()
+        while not rospy.is_shutdown():
+            #print(self.pose)
+            print(mouseToXY(self.getRelativeMousePosition(getCurrentMousePosition())))
 
 
 if __name__ == "__main__":
